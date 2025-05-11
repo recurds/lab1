42c24206eca68bf92b69f46c78988480b07ea7f7
Taras Besh <tarasbesh123@fmail.com>
2025-05-11 22:48:41 +0300

    lab 1

---
+++ src/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"HelloWorldApp","l":"HelloWorldApp()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"HelloWorldApp","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
